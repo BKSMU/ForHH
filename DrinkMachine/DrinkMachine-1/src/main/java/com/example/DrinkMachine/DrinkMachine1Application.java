071e727643b86c261be6403de70c39b189014a94
hayashihiro <h_hayashi@bs-ja.co.jp>
2022-10-26 00:48:44 +0900

    10/25

---
+++ DrinkMachine/DrinkMachine-1/src/main/java/com/example/DrinkMachine/DrinkMachine1Application.java
@@ -0,0 +1,14 @@
+package com.example.DrinkMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkMachine1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkMachine1Application.class, args);
+	}
+
+}
+
